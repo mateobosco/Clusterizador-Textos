5a35b9c62945bf120a20f0bcc657bc6612ed9710
mateo.bosco@gmail.com <mateo.bosco@gmail.com@4b9898d7-62f8-961e-5752-632e6e541a77>
2013-11-01 19:11:59 +0000

    ahora el creador de shingles anda bien, falta solo ver bien el tema del EOF

--- datos2013/shingles.c
+++ datos2013/shingles.c
@@ -7,15 +7,8 @@ int creador_shingles(char* nombre_archivo, int tamano){
 	remove("salida");
 	FILE* archivo;
 	FILE* salida;
-	int contadores[7];
-	char* shingles[7];
-	int i;
-	for (i = 0; i < tamano; i++){
-		contadores[i] = i;
-		char shingle[7] = "*******";
-		shingles[i] = shingle;
-	}
-
+	char shingle_old[7] = "*******";
+	char shingle_new[7] = "*******";
 	archivo = fopen(nombre_archivo , "r");
 	salida = fopen("salida", "a");
 	if ( archivo == NULL){
@@ -23,37 +16,30 @@ int creador_shingles(char* nombre_archivo, int tamano){
 		return -1; //ERROR DE LECTURA
 	}
 
-	i = 0;
+
 	while (feof(archivo) == 0){
-		int j;
-		for (j = 0 ; j < tamano ; j++){
-			if (contadores[j] >= tamano) contadores[j] = 0; //REINICIAR CONTADORES
-		}
 		char caracter;
 		caracter = fgetc(archivo);
 		printf("LEO ESTE CARACTER %c \n", caracter);
-
-		for (j = 0 ; j < tamano ; j++){
-			shingles[j][contadores[j]] = caracter; //MODIFICAR SHINGLES
+		int i;
+		for (i = 1 ; i < tamano ; i ++){
+			shingle_new[i-1] = shingle_old[i];
+			shingle_old[i-1] = shingle_new[i-1];
 		}
+		shingle_new[tamano - 1] = caracter;
+		shingle_old[tamano - 1] = caracter;
 
-		for (j = 0 ; j < tamano ; j++){
-			if (shingles[j][0] != '*' && shingles[j][tamano-1] != '*' ){
-				fputs( shingles[j] , salida );//ESCRIBIR SHINGLES EN SALIDA
+		printf("ESTE ES SHINGLE NEW %s", shingle_new);
+		if (shingle_new[0] != '*' && shingle_new[tamano-1] != '*' ){
+				fputs( shingle_new , salida );//ESCRIBIR SHINGLES EN SALIDA
 				fputs( "\n" , salida);
-			}
 		}
-		for (j = 0 ; j < tamano ; j++){
-			contadores[j] ++ ; //AUMENTO CONTADORES
-		}
-
 
 	}
 
 	fclose(archivo);
 	return 0;
 }
-
 
 
 
