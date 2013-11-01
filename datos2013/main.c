5178dc77bbcf4a1efd283ec05b64a8a25dedf782
mateo.bosco@gmail.com <mateo.bosco@gmail.com@4b9898d7-62f8-961e-5752-632e6e541a77>
2013-11-01 15:04:00 +0000

    agrega el nombre de los archivos al relativo usa lista auxiliar

--- datos2013/main.c
+++ datos2013/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
-#include <stdbool.h> //LIBRERIA PARA BINARIO AVERIGUAR
+#include <stdbool.h> //LIBRERIA PARA BINARIO, AVERIGUAR
 #include "relativo.h"
+#include "lista.h"
+
+#define NELEMS(x)  (sizeof(x) / sizeof(x[0]))
+
 
 int main( int argc, char *argv[] ){
 	DIR *dir;
@@ -18,15 +22,41 @@ int main( int argc, char *argv[] ){
 		exit( -1 );
 	}
 
-	int cantidad;
+	int cantidad = 0;
+	int long_max = 1;
+	lista_t* lista_aux;
+	lista_aux = lista_crear();
 
 	while( (mi_dirent = readdir( dir )) != NULL ){
+		int aux = NELEMS(mi_dirent->d_name);
+		if (aux > long_max) long_max = aux;
 		if(mi_dirent->d_name[0]!= '.'){ //NEGRADA. Y PROBLEMA SI HAY UNA CARPETA
 			printf( "%s\n", mi_dirent->d_name );
+			bool res = lista_insertar_ultimo(lista_aux, mi_dirent->d_name);
+
 			cantidad ++;
 		}
 	}
 	printf("cantidad de archivos %d \n", cantidad);
+	printf("largo de la lista %d \n", (int) lista_largo(lista_aux));
+
+
+	int rel_nombres;
+	char nombres[] = "relativo_nombres";
+	rel_nombres = R_CREATE(nombres, long_max, cantidad);
+
+	lista_iter_t* mi_iterador = lista_iter_crear(lista_aux);
+	while( !lista_iter_al_final(mi_iterador) ){
+		printf("entra al while \n");
+		int res = R_WRITE (rel_nombres, cantidad, lista_iter_ver_actual(mi_iterador)); // CONTROLAR ESCRITURA
+		printf( "ESCRIBO EN EL RELATIVO EN LA POSICION %d %s\n", cantidad, (char*) lista_iter_ver_actual(mi_iterador) );
+		lista_iter_avanzar(mi_iterador);
+	}
+	lista_iter_destruir(mi_iterador);
+	lista_destruir(lista_aux, NULL);
+	int res = R_DELETE (rel_nombres, cantidad); //NO SE QUE MIERDA HACE
+	remove("relativo_nombres"); //MEDIO VISHERO PERO ANDA
+
 	closedir( dir );
 
 	return 0;
