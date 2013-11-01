9132c48959395911cdcd2ee6845e476d0928efb1
mateo.bosco@gmail.com <mateo.bosco@gmail.com@4b9898d7-62f8-961e-5752-632e6e541a77>
2013-11-01 04:54:42 +0000

    lee nombre de archivos, problema si hay una carpeta

--- datos2013/main.c
+++ datos2013/main.c
@@ -1,6 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <stdbool.h> //LIBRERIA PARA BINARIO AVERIGUAR
 
+int main( int argc, char *argv[] ){
+	DIR *dir;
+	struct dirent *mi_dirent;
 
+	if( argc != 2 ){
+		printf( "%s: %s directorio\n", argv[0], argv[0] );
+		exit( -1 );
+	}
 
-int main(void){
+	if( (dir = opendir( argv[1] )) == NULL ){
+		perror( "opendir" );
+		exit( -1 );
+	}
+
+	int cantidad;
+
+	while( (mi_dirent = readdir( dir )) != NULL ){
+		if(mi_dirent->d_name[0]!= '.'){ //NEGRADA. Y PROBLEMA SI HAY UNA CARPETA
+			printf( "%s\n", mi_dirent->d_name );
+			cantidad ++;
+		}
+	}
+	printf("cantidad de archivos %d \n", cantidad);
+	closedir( dir );
+
 	return 0;
 }
