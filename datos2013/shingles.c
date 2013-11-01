8671172b2665e6c6ed2fed86981b3898df89562a
mateo.bosco@gmail.com <mateo.bosco@gmail.com@4b9898d7-62f8-961e-5752-632e6e541a77>
2013-11-01 19:48:37 +0000

    hay que ir metiendo los shingles en un heap. ver que pasa con el tamano del heap y hay un warning en la funcion de cmp

--- datos2013/shingles.c
+++ datos2013/shingles.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include "heap.h"
 
+int mi_cmp(char* string1, char* string2){
+	if (string1 < string2) return 1;
+	if (string2 < string1) return -1;
+	else return 0;
+}
 
 int creador_shingles(char* nombre_archivo, int tamano){
 	remove("salida");
@@ -15,31 +21,36 @@ int creador_shingles(char* nombre_archivo, int tamano){
 		printf("ERROR DE LECTURA \n");
 		return -1; //ERROR DE LECTURA
 	}
-
+	heap_t* mi_heap = heap_crear(mi_cmp);
 
 	while (feof(archivo) == 0){
-		char caracter;
-		caracter = fgetc(archivo);
-		printf("LEO ESTE CARACTER %c \n", caracter);
 		int i;
 		for (i = 1 ; i < tamano ; i ++){
 			shingle_new[i-1] = shingle_old[i];
 			shingle_old[i-1] = shingle_new[i-1];
 		}
+		char caracter;
+		caracter = fgetc(archivo);
+		while (caracter == '.' && caracter == ','){ //COMPROBAR QUE NO ESTE DENTRO DE UNA LISTA DE CARACTERES PROHIBIDOS
+			caracter = fgetc(archivo);
+		}
+		printf("LEO ESTE CARACTER %c \n", caracter);
 		shingle_new[tamano - 1] = caracter;
 		shingle_old[tamano - 1] = caracter;
 
 		printf("ESTE ES SHINGLE NEW %s", shingle_new);
-		if (shingle_new[0] != '*' && shingle_new[tamano-1] != '*' ){
+		if (shingle_new[0] != '*' && shingle_new[tamano-1] != '*' && caracter != EOF ){
 				fputs( shingle_new , salida );//ESCRIBIR SHINGLES EN SALIDA
 				fputs( "\n" , salida);
 		}
 
 	}
 
+	heap_destruir(mi_heap, NULL);
 	fclose(archivo);
 	return 0;
 }
+
 
 
 
